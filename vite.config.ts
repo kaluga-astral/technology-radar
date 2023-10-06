748355f8c5a6809c517cf5571a7cfa7d741f0510
Andrey Potyomkin <and_tem@icloud.com>
2023-10-06 17:06:33 +0300

    chore: Fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.VITE_BASE_APTH,
+  base: '/technology-radar',
   plugins: [react()],
 })
