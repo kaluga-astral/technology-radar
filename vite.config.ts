916565655cfe86bebc36b0a4b9dfdfa91096df52
Andrey Potyomkin <and_tem@icloud.com>
2023-10-06 17:01:30 +0300

    chore: Поправлены ссылки приложения

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/technology-radar',
+  base: process.env.VITE_BASE_APTH,
   plugins: [react()],
 })
