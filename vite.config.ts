e642d3e68a1a504ae8df9341ae2b991679723cf3
Andrey Potyomkin <and_tem@icloud.com>
2023-10-06 17:07:30 +0300

    chore: Добавил TODO

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // TODO: сделать так, чтобы base забирался из process.env
   base: '/technology-radar',
   plugins: [react()],
 })
